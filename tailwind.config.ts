1630e13dbd0f5150f2f636b5c4b5393e6eb8601f
Anil Antony <anilantony3134@gmail.com>
2023-06-02 08:23:13 +0530

    feat: added colors tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#dc2626'
+      }
+    },
   },
   plugins: [require("daisyui")],
 } satisfies Config;
